ca0ec75f6199c015bd46922c72ed880e3ba0eb69
Bhakti <bhaktinirbhaybhatt@gmail.com>
2023-07-22 12:13:04 +0530

    added username and password using dotenv

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+const USERNAME = process.env.DB_USERNAME;
+const PASSWORD = process.env.DB_PASSWORD;
 
 const Connection = () => {
-    const MONGODB_URI = 'mongodb://user1:passwordB@ac-dqxwodw-shard-00-00.wdaurhm.mongodb.net:27017,ac-dqxwodw-shard-00-01.wdaurhm.mongodb.net:27017,ac-dqxwodw-shard-00-02.wdaurhm.mongodb.net:27017/?ssl=true&replicaSet=atlas-7rwg63-shard-0&authSource=admin&retryWrites=true&w=majority'
+    const MONGODB_URI = `mongodb://${USERNAME}:${PASSWORD}@ac-dqxwodw-shard-00-00.wdaurhm.mongodb.net:27017,ac-dqxwodw-shard-00-01.wdaurhm.mongodb.net:27017,ac-dqxwodw-shard-00-02.wdaurhm.mongodb.net:27017/?ssl=true&replicaSet=atlas-7rwg63-shard-0&authSource=admin&retryWrites=true&w=majority`;
     mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
 
     mongoose.connection.on('connected', () => {
