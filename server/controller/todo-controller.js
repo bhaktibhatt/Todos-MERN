94495089fa5f7a2f2bad0ba1deeb7b50d70f3fbd
Bhakti <bhaktinirbhaybhatt@gmail.com>
2023-07-23 20:48:22 +0530

    inputs getting saved succesfully in mongoDb

--- server/controller/todo-controller.js
+++ server/controller/todo-controller.js
@@ -1,5 +1,16 @@
+// import { response } from 'express';
+import Todo from '../model/Todo.js';
 
-
-export const addTodo =  (request, response) => {
-    console.log(request.body);
+export const addTodo =  async (request, response) => {
+    try{
+        const newTodo = await Todo.create({
+            data: request.body.data,
+            createdAt: Date.now()
+        })
+        await newTodo.save();
+        
+        return response.status(200).json(newTodo);
+    } catch (error) {
+        return response.status(500).json(error.message)
+    }
 }
