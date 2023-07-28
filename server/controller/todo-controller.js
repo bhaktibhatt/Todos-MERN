9d544f166129016c99a09004dfca4665f7c5dcb9
Bhakti <bhaktinirbhaybhatt@gmail.com>
2023-07-28 14:06:34 +0530

    connecting getAllTodos API

--- server/controller/todo-controller.js
+++ server/controller/todo-controller.js
@@ -14,3 +14,17 @@ export const addTodo =  async (request, response) => {
         return response.status(500).json(error.message)
     }
 }
+
+export const getAllTodos = async () =>{
+    try{
+        const newTodo = await Todo.create({
+            data: request.body.data,
+            createdAt: Date.now()
+        })
+        await newTodo.save();
+        
+        return response.status(200).json(newTodo);
+    } catch (error) {
+        return response.status(500).json(error.message)
+    }
+}
--- server/router/route.js
+++ server/router/route.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { addTodo } from '../controller/todo-controller.js';
+import { getAllTodos } from '../../todos-app/src/redux/actions/index.js';
 const route = express.Router();
-route.post('/todos', addTodo)
-
+route.post('/todos', addTodo) //function that posts todo on database
+route.get('/todos', getAllTodos) //function that gets todos
 export default route
